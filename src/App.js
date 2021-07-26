c408e0eb8f026ed2e6023683a503cf86b7d726a2
mandykparson <79546411+mandykparson@users.noreply.github.com>
2021-07-26 10:37:52 -0600

    adding some styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import AboutMe from './Components/AboutMe';
+import Blog from './Components/Blog';
+import Header from './Components/Header';
+import Projects from './Components/Projects';
+import Resume from './Components/Resume';
+import { useState, useRef } from 'react'
 
 function App() {
+
+  const [ isAboutMeOpen, setIsAboutMeOpen] = useState(false)
+  const [ isProjectsOpen, setIsProjectsOpen] = useState(false)
+  const [ isResumeOpen, setIsResumeOpen] = useState(false)
+  const [ isBlogOpen, setIsBlogOpen] = useState(false)
+
+  const aboutMeRef = useRef(null)
+  const projectsRef = useRef(null)
+  const resumeRef = useRef(null)
+  const blogRef = useRef(null)
+
+  const toggleAboutMe = () => {
+    setIsAboutMeOpen(!isAboutMeOpen)
+  }
+  const toggleProjects = () => {
+    setIsProjectsOpen(!isProjectsOpen)
+  }
+  const toggleResume = () => {
+    setIsResumeOpen(!isResumeOpen)
+  }
+  const toggleBlog = () => {
+    setIsBlogOpen(!isBlogOpen)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="main-content">
+        <div className="slider" ref={aboutMeRef}className="about-me" onClick={toggleAboutMe}>
+          <AboutMe />
+        </div>
+        <div className="slider" className="projects" onClick={toggleProjects}>
+          <Projects />
+        </div>
+        <div className="slider" className="resume" onClick={toggleResume}>
+          <Resume />
+        </div>
+        <div className="slider" className="blog" onClick={toggleBlog}>
+          <Blog />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <div>
+            <h1>Mandy Parson</h1>
+            <h4>Full Stack Software Engineer</h4>
+        </div>
+    )
+}
